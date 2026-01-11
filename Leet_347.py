f30556409751530bb6aa84e956d83d1b8dc5e19f
EZDevanshu <devanshuparmar00@gmail.com>
2026-01-11 11:13:49 +0530

    Hashmap_Leet_pattern

---
+++ Leet_347.py
@@ -0,0 +1,15 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        di = {}
+        res = []
+
+        for i in nums:
+            if not i in di :
+                di[i] = 1
+            else :
+                di[i] += 1
+        sorted_dict = sorted(di.items(), key = lambda x : x[1] , reverse = True)
+
+        res = [i[0] for i in sorted_dict[:k]]
+        return res
+    
