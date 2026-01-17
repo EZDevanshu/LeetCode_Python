6e4bee454f0ec30ab8ad2a88b3e8a920e43fa9b8
EZDevanshu <devanshuparmar00@gmail.com>
2026-01-17 10:35:30 +0530

    changed

--- Leet_36.py
+++ Leet_36.py
@@ -7,7 +7,7 @@ class Solution:
         for r in range(9):
             
             for c in range(9):
-                if board[r][c] == " ":
+                if board[r][c] == ".":
                     continue
                 if (board[r][c] in cols[c] or 
                     board[r][c] in rows[r] or 
