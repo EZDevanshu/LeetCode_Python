5e48dfa19b02a9c870e19fb70ce6fa03e52add96
EZDevanshu <devanshuparmar00@gmail.com>
2026-06-08 18:03:33 +0530

    implement consecutiveSetBits solution to determine if a number has exactly one group of consecutive set bits

---
+++ Leet_3950.py
@@ -0,0 +1,13 @@
+class Solution:
+    def consecutiveSetBits(self, n: int) -> bool:
+        count = 0
+        while n > 0 :
+
+            if (n & 3) == 3 :
+                count+= 1
+            
+            if count > 1 :
+                return False
+            
+            n>>=1 
+        return count == 1
