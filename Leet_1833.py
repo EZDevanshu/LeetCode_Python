2f6c941d74380f548e843d896d96530b65b49357
EZDevanshu <devanshuparmar00@gmail.com>
2026-06-22 18:06:28 +0530

    implement maxIceCream solution to calculate the maximum number of ice creams that can be bought with a given amount of coins

---
+++ Leet_1833.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs.sort()
+
+        count = 0
+        i = 0
+        while coins != 0 and i < len(costs) :
+            if costs[i] <= coins :
+                count += 1
+                coins -= costs[i]
+            i+= 1
+        
+        return count
