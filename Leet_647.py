64b01cd3d1e805102eea51f0c8d64303b1ad6b5e
EZDevanshu <devanshuparmar00@gmail.com>
2026-06-23 19:02:05 +0530

    refactor countSubstrings solution to implement an optimal approach for counting palindromic substrings

--- Leet_647.py
+++ Leet_647.py
@@ -1,16 +1,44 @@
+# brute force approch 
+
+# class Solution:
+#     def countSubstrings(self, s: str) -> int:
+#         li = []
+
+#         for i in range(len(s)) :
+#             res = ""
+#             for j in range(i , len(s)) :
+#                 res += s[j]
+#                 li.append(res)
+
+#         count = 0
+#         for x in li :
+#             if x[::-1] == x :
+#                 count += 1    
+
+#         return count  
+
+
+# optimal approch 
+
 class Solution:
-    def countSubstrings(self, s: str) -> int:
-        li = []
-
-        for i in range(len(s)) :
-            res = ""
-            for j in range(i , len(s)) :
-                res += s[j]
-                li.append(res)
-
+    def countSubstrings(self, s: str) -> int :
         count = 0
-        for x in li :
-            if x[::-1] == x :
-                count += 1    
 
-        return count  
+        def checkPalindrome(l , r) :
+            noOfPalin = 0
+            while l >= 0 and r < len(s) and s[l] == s[r] :
+                noOfPalin += 1
+                l -= 1
+                r += 1
+
+            return noOfPalin
+        
+        for i in range(len(s)) :
+            count += checkPalindrome(i , i)
+            count += checkPalindrome(i , i + 1)
+
+        return count
+    
+      
+     
+      
