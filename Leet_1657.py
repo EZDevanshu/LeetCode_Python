b59d94f30982a0196c0d2f2f8a073b04019b9fb1
EZDevanshu <devanshuparmar00@gmail.com>
2026-06-26 18:57:05 +0530

    implement closeStrings function to determine if two strings can be made similar

---
+++ Leet_1657.py
@@ -0,0 +1,21 @@
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if len(word1) != len(word2) :
+            return False
+
+        d1 = {}
+        d2 = {}
+
+        for i in range(len(word1)) :
+            d1[word1[i]] = d1.get(word1[i] , 0) + 1
+        
+        for i in range(len(word2)) :
+            d2[word2[i]] = d2.get(word2[i] , 0) + 1
+           
+        if set(d1.keys()) != set(d2.keys()) :
+            return False 
+        
+        if sorted(d1.values()) != sorted(d2.values()) :
+            return False
+         
+        return True 
