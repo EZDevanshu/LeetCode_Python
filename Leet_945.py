4d48e61adb705e056d158bed7830d4ca33fe1c7c
EZDevanshu <devanshuparmar00@gmail.com>
2026-06-30 19:45:31 +0530

    implement minIncrementForUnique function to ensure all elements in the list are unique by incrementing duplicates

---
+++ Leet_945.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        nums.sort()
+        totalInc = 0
+
+        for i in range(1 , len(nums)) :
+            if nums[i] <= nums[i - 1] :
+                totalInc += nums[i - 1] + 1 - nums[i]   
+                nums[i] = nums[i - 1] + 1
+        return totalInc
+
+
+                
