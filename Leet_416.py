a868b28e9cbc96c0737b18e1b002abc3b95eebbb
EZDevanshu <devanshuparmar00@gmail.com>
2026-07-04 12:57:53 +0530

    implement canPartition function to determine if an array can be partitioned into two subsets with equal sum

---
+++ Leet_416.py
@@ -0,0 +1,31 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        
+        total = sum(nums)
+        n = len(nums)
+        
+        if total % 2 != 0 :
+            return False
+
+        target = total // 2 
+    
+        def isTrue(arr , n , sum) :
+            dp = [[False] * (sum + 1) for _ in range(n + 1)] 
+        
+            for i in range(n + 1) :
+                for j in range(sum + 1) : 
+                    if i == 0 and j == 0 :
+                        dp[i][j] = True
+                    elif j == 0 : dp[i][j] = True
+                    elif i == 0 : dp[i][j] = False
+
+            for i in range(1, n + 1) :
+                for j in range(1,sum + 1) :
+                    
+                    if arr[i - 1] <= j :
+                        dp[i][j] = dp[i - 1][j - arr[i - 1]] or dp[i - 1][j]
+                    else :
+                        dp[i][j] = dp[i -1][j]
+
+            return dp[n][sum]
+        return isTrue(nums , n , target)
