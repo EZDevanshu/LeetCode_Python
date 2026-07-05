29209ceae062925634b84ac5317236d3eb0d3bf7
EZDevanshu <devanshuparmar00@gmail.com>
2026-07-05 18:58:18 +0530

    implement perfectSum function to calculate the number of ways to achieve a target sum from an array

---
+++ GFG.py
@@ -0,0 +1,28 @@
+class Solution:
+	def perfectSum(self, arr, target):
+		# code here
+		n = len(arr) 
+		
+		def knapsack(arr , n , target) :
+		    dp = [[0] * (target + 1) for _ in range(n + 1)] 
+		    
+		    for i in range(n + 1) :
+		        for j in range(target + 1) :     
+		            if i == 0 and j == 0 :
+        		        dp[i][j] = 1
+        		    elif i == 0 :
+        		        dp[i][j] =  0
+        		    elif j == 0 :
+        		        dp[i][j] = 1
+        		       
+        	for i in range(1, n + 1) :
+        	    for j in range(1, target + 1) :
+        	        
+        	        if arr[i - 1] <= j :
+        	            dp[i][j] = dp[i - 1][j - arr[i - 1]] + dp[i - 1][j]
+        	       
+        	        else :
+        	            dp[i][j] = dp[i - 1][j]
+        	       
+        	return dp[n][target]
+        return knapsack(arr , n , target)
