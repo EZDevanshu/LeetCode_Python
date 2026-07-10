806197a7600c8d5d5f915a5985bdbe470c14a9cb
EZDevanshu <devanshuparmar00@gmail.com>
2026-07-10 20:04:38 +0530

    implement lastStoneWeightII function to calculate the minimum possible weight of the last stone

---
+++ Leet_1049.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lastStoneWeightII(self, arr: List[int]) -> int:
+        n = len(arr)
+        total = sum(arr)
+        dp = {}
+        def backtrack(i , sum1) :
+            if i == n :
+                return abs(total - 2 * sum1)
+
+            if (i, sum1) in dp:
+                return dp[(i, sum1)]
+
+            take = backtrack(i + 1 , sum1 + arr[i])
+            nonTake = backtrack(i + 1 ,sum1)
+
+            dp[(i , sum1)] =  min(take , nonTake)
+
+            return dp[(i , sum1)]
+        return backtrack(0,0)   
