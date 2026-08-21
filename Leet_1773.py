d770d8d7206da161fadf7a1573f8cd2d58081c0f
EZDevanshu <devanshuparmar00@gmail.com>
2026-08-21 12:39:36 +0530

    add countMatches function to count items based on specified rule key and value

---
+++ Leet_1773.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        count = 0
+        if ruleKey == "type" :
+            for i in items :
+                if i[0] == ruleValue :
+                    count += 1
+            return count
+            
+        if ruleKey == "color" :
+            for i in items :
+                if i[1] == ruleValue :
+                    count += 1
+            return count
+            
+        if ruleKey == "name" :
+            for i in items :
+                if i[2] == ruleValue :
+                    count += 1
+            return count
+                
+        
+            
